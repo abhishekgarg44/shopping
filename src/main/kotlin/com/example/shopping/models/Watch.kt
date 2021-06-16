102ff5569bbcc3a154e4417e00a4cbf7715ca7e5
Abhishek Garg <abhishgarg@expediagroup.com>
2021-06-16 20:19:21 +0530

    removing extra file

--- src/main/kotlin/com/example/shopping/models/Watch.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shopping.models
-
-class Watch {
-    var watchId: Int? = null
-    var watchName: String? = null
-    var unitPrice: Int? = null
-    var discount: String? = null
-}
-
